5dca12100c36f7488209f512d9bf0a5bc219b2b0
Ganjar Sugiharto <ganjar.su15@gmail.com>
2024-01-23 16:31:06 +0700

    tugas akhir
    commit

---
+++ src/components/MethodComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <hr />
+      <div class="alert" :class="{ 'show': isAlertVisible }">
+        {{ alertMessage }}
+      </div>
+      <button class="btn btn-primary w-25" @click="showWelcomeMessage">Klik Aku dong !</button>
+      <hr />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "MethodComponent",
+    data() {
+      return {
+        isAlertVisible: false,
+        alertMessage: "Ganjar Sugiharto peserta Bootcamp kelas Front End di GameLab",
+      };
+    },
+    methods: {
+      toggleAlert() {
+        this.isAlertVisible = !this.isAlertVisible;
+      },
+      showWelcomeMessage() {
+        this.toggleAlert();
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Gaya CSS untuk komponen */
+  .alert {
+    background-color: #f8d7da;
+    color: #721c24;
+    padding: 10px;
+    border: 1px solid #f5c6cb;
+    border-radius: 5px;
+    opacity: 0;
+    transition: opacity 0.5s;
+    cursor: pointer;
+  }
+  
+  .show {
+    opacity: 1;
+  }
+  </style>
+  
---
+++ src/components/NavigasiComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+    <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#home" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#katalog" class="nav-link">Products</a></li>
+            <li class="nav-item"><a href="#testimoni" class="nav-link">Testimoni</a></li>
+            <li class="nav-item"><a href="#faqs" class="nav-link">FAQs</a></li>
+        </ul>
+    </header>
+  </div>
+</template>
+<script>
+    export default{
+        name: 'NavigasiComponent',
+    }
+</script>
