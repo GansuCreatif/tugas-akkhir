7fc7d21f83b663a374f993c81565a7a8b1bc5840
Ganjar Sugiharto <ganjar.su15@gmail.com>
2024-01-24 15:16:31 +0700

    commit

--- src/components/OrderComponent.vue
+++ src/components/OrderComponent.vue
@@ -40,5 +40,92 @@
   
     }
 }
+@media (min-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    
+    header {
+      padding: 15px;
+    }
+  
+    .nav-pills {
+      flex-direction: row;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
+  
+  @media (max-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 60px 0;
+    }
+  }
+  
+  @media (max-width: 576px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 1px;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+    #faqs{
+      flex-direction: column;
+    }
+  }
 
+  @media (max-width:400px){
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 2.5rem;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
 </style>
--- src/components/TestimoniComponent.vue
+++ src/components/TestimoniComponent.vue
@@ -11,7 +11,7 @@
           <div class="carousel-item active">
                 <div class="container">
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-lg-4">
                             <img src="../assets/avatar.png" class="bd-placeholder-img rounded-circle" width="100" height="100"/>
 
                             <h2>John Doe</h2>
@@ -133,10 +133,10 @@
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@1,700&family=Oswald:wght@200;700&display=swap');
 #testimoni{
- background-color: #d0d0d0;
+ background-color: #aafaaf;
  margin-bottom: 40px;
     h2{
-      font-size: 80px;
+      font-size: 30px;
       color: #050505;
       font-family: 'Chakra Petch', sans-serif;
       font-weight: bold;
@@ -144,6 +144,182 @@
     }
     
 }
+@media (min-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    
+    header {
+      padding: 15px;
+    }
+  
+    .nav-pills {
+      flex-direction: row;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
+  
+  @media (max-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 60px 0;
+    }
+  }
+  
+  @media (max-width: 576px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 1px;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+    #faqs{
+      flex-direction: column;
+    }
+  }
+
+  @media (max-width:400px){
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 2.5rem;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
+  @media (min-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    
+    header {
+      padding: 15px;
+    }
+  
+    .nav-pills {
+      flex-direction: row;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
+  
+  @media (max-width: 992px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 60px 0;
+    }
+  }
+  
+  @media (max-width: 576px) {
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 1px;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+    #faqs{
+      flex-direction: column;
+    }
+  }
+
+  @media (max-width:400px){
+    .rounded-lg-3 { border-radius: .3rem; }
+    section {
+      padding: 40px 0;
+    }
+  
+    h1 {
+      font-size: 2.5rem;
+    }
+  
+    h2 {
+      font-size: 2rem;
+    }
+  
+    p {
+      font-size: 1rem;
+    }
+  
+    footer {
+      padding: 15px;
+    }
+    .nav-pills {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .nav-pills .nav-link {
+      margin-bottom: 10px;
+    }
+  }
 </style>
 <script>
  export default{
@@ -154,4 +330,5 @@
             };
         }
     }
+    
 </script>
